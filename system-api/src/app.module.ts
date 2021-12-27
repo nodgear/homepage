6e7806593d760875f83b681d06ebc88ec8cb20f7
Silasoli <silas.oliveira@blockhub.builders>
2021-12-27 01:36:54 -0300

    add initial config with bd uri

--- system-api/src/app.module.ts
+++ system-api/src/app.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        let uri: any;
+        uri = configService.get<string>('DB_URL');
+        if (!uri) throw new Error('banco de dados não configurado.');
+        return { uri };
+      },
+      inject: [ConfigService],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
