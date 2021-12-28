28f470dc5258003f91f25769bf4903ab55212e7a
Nodge <matheus.silva3@ftc.edu.br>
2021-12-28 14:40:41 -0300

    feat: chavepix + ong display

--- system-app/components/sections/QuemSomos.tsx
+++ system-app/components/sections/QuemSomos.tsx
@@ -19,8 +19,16 @@ const QuemSomos = () => {
            //TODO: Alguém faz o modal <3
         }} />
       </div>
-      <div className='flex flex-col w-full'>
-
+      <div className='flex flex-col items-center w-full'>
+        <div className='flex flex-col items-center justify-center text-2xl text-center'>
+          <h1 className='font-bold uppercase text-accent'>Quero contribuir</h1>
+          <p>Chave pix: {config.chavePix}</p>
+          <img src="/qrpix.png" className='px-2 py-2 mt-6 bg-white rounded-md w-38' />
+        </div>
+        <div className='flex flex-col items-center justify-center m-6 text-2xl text-center'>
+          <h1 className='font-bold uppercase text-accent'>Quem estamos ajudando?</h1>
+          <img src="/ong.png" className='mt-6 w-38' />
+        </div>
       </div>
     </section>
   );
