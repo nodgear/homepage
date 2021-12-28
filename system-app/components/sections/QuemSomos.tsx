7a9f73178b78561faf225cdc439fa41fc6b72b41
Nodge <matheus.silva3@ftc.edu.br>
2021-12-28 14:43:30 -0300

    fix: quemsomos proportion

--- system-app/components/sections/QuemSomos.tsx
+++ system-app/components/sections/QuemSomos.tsx
@@ -6,7 +6,7 @@ import config from '../../config.json'
 const QuemSomos = () => {
   return (
     <section className='flex flex-row items-center h-screen px-20'>
-      <div className='flex flex-col w-2/3'>
+      <div className='flex flex-col w-1/2'>
         <h1 className='w-full px-3 mb-12 text-2xl font-bold text-center uppercase text-accent'>Quem somos</h1>
         <p className='px-3 mb-2 text-lg'>
           Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. 
@@ -19,7 +19,7 @@ const QuemSomos = () => {
            //TODO: Alguém faz o modal <3
         }} />
       </div>
-      <div className='flex flex-col items-center w-full'>
+      <div className='flex flex-col items-center w-1/2'>
         <div className='flex flex-col items-center justify-center text-2xl text-center'>
           <h1 className='font-bold uppercase text-accent'>Quero contribuir</h1>
           <p>Chave pix: {config.chavePix}</p>
