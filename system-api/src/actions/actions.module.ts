a65736d38cfda77bbd3194a8454afd22fa326d70
Silasoli <silas.oliveira@blockhub.builders>
2021-12-28 02:09:22 -0300

    add validation files and import mongoose in action

--- system-api/src/actions/actions.module.ts
+++ system-api/src/actions/actions.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ActionsService } from './actions.service';
 import { ActionsController } from './actions.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Action, ActionSchema } from './entities/action.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Action.name, schema: ActionSchema }
+    ]),
+  ],
   controllers: [ActionsController],
   providers: [ActionsService]
 })
